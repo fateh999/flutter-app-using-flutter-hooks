09f6a0ee3824e388d33210c32af5d25e86296bf6
fateh999 <farooquifateh184@gmail.com>
2020-03-14 14:40:46 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:demo_app/Screens/Counter/CounterScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'Screens/Login/LoginScreen.dart';
@@ -8,14 +7,12 @@ void main() => runApp(DemoApp());
 class DemoApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final loggedIn = useState(false);
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: loggedIn.value ? CounterScreen() : LoginScreen(),
+      home: LoginScreen(),
     );
   }
 }
